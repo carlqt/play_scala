9759058acf98c121d371e2e753c1eda58b24545d
Carl Tablante <carlwilliam.tablante@gmail.com>
2015-11-17 17:44:23 +0800

    added flash message

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -2,8 +2,18 @@ package controllers
 
 import play.api.mvc._
 import models.Product
+import play.api.data.Form
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 
 class Products extends Controller {
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply)
+  )
+
   def list = Action { implicit request =>
 
     val products = Product.findAll
@@ -20,6 +30,6 @@ class Products extends Controller {
   }
 
   def newProduct = Action { implicit request =>
-    Ok(views.html.products.newProduct())
+    Ok(views.html.products.newProduct(request.flash))
   }
 }
